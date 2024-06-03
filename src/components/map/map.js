ec0f66a654cc446962d025d304b3f4376b39584e
rjasmin-camsys <rjasmin@camsys.com>
2024-06-03 16:42:33 -0400

    adding in QA env variables
    adding in QA env variables

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -90,7 +90,7 @@ const map = (function() {
                 useEffect(() => {
                     (async () => {
                         const response = await fetch(
-                            "https://" + process.env.DEV_ENV_ADDRESS + process.env.VEHICLE_MONITORING_ENDPOINT + search
+                            "https://" + process.env.QA_ENV_ADDRESS + process.env.VEHICLE_MONITORING_ENDPOINT + search
                         );
                         const parsed = await response.json();
                         setVehicles(parsed.Siri.ServiceDelivery.VehicleMonitoringDelivery[0].VehicleActivity);
