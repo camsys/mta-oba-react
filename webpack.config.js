eba1e0649a67fe4588292700a9ffca629c4a3aeb
CaylaSavitzky <41533582+CaylaSavitzky@users.noreply.github.com>
2024-06-04 15:51:37 -0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const htmlPlugin = new HtmlWebPackPlugin({
  filename: "./index.html"
 });
 const envPlugin = new webpack.DefinePlugin({
-    'process.env.ALLOWED_HOST_ADDRESS': JSON.stringify(process.env.ALLOWED_HOST_ADDRESS || 'app.qa.obanyc.com/'),
+    'process.env.ALLOWED_HOST_ADDRESS': JSON.stringify(process.env.ALLOWED_HOST_ADDRESS || 'app-react.qa.obanyc.com/'),
     'process.env.ENV_ADDRESS': JSON.stringify(process.env.ENV_ADDRESS || 'app.qa.obanyc.com/'),
     'process.env.VEHICLE_MONITORING_ENDPOINT': JSON.stringify(process.env.VEHICLE_MONITORING_ENDPOINT || 'api/siri/vehicle-monitoring.json?key=OBANYC&_=1707407738784&OperatorRef=MTA+NYCT')
 });
