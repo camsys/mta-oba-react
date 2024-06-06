b0c55c65052a2ff5807fe142f70f3359a5d51260
caylasavitzky <caysavitzky@gmail.com>
2024-06-06 13:22:58 -0400

    adding to stopData model

--- src/js/updateState/dataModels.js
+++ src/js/updateState/dataModels.js
@@ -1,5 +1,7 @@
 export class stopData {
     constructor(stopJson) {
         this.name = stopJson.name
+        this.longLat = [stopJson.longitude,stopJson.latitude]
+        this.id = stopJson.id
     }
 }
