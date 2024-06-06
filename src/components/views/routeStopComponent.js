764f3e692744401f133fdf504258240ef70a7420
caylasavitzky <caysavitzky@gmail.com>
2024-06-06 14:08:52 -0400

    break routeCard into components

--- src/components/views/routeStopComponent.js
+++ src/components/views/routeStopComponent.js
@@ -1,6 +1,11 @@
 import React from "react";
 
 function routeStopComponent  (stopData) {
-    return (<span></span>)
+    return (
+        <li className="has-info">
+            <a href="#" tabIndex="-1">Seneca Av/DeKalb Av</a>
+            <routeVehicleComponent/>
+        </li>
+    )
 }
 export default routeStopComponent;
---
+++ src/components/views/routeVehicleComponent.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+return (<span/>)
+//     <ul className="approaching-buses">
+//         <li>
+//             <a
+//                 href="/?content=route-b-38_vehicle&search=B38&refresh=true&vehicle-popup=true"
+//                 className="bus">7619</a>
+//             <span className="bus-info">
+//                               <span className="approaching">&lt;1 stop away</span>
+//                               <span className="passengers">~16 passengers</span>
+//                             </span>
+//         </li>
+//     </ul>
