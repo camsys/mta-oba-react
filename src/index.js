95b1ec380ed19e7f0a7ca18abaa32ebef44e91dd
caylasavitzky <caysavitzky@gmail.com>
2024-06-07 10:14:38 -0400

    actually using Becky's javascript

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import React from 'react';
 import { useHistory, useLocation } from 'react-router';
 import { ReactSVG } from "react-svg";
 import './img/favicon.ico';
-// import './css/bustime.css';
 import { createRoot } from 'react-dom/client';
 import "leaflet/dist/leaflet.css";
 import { OBA } from './js/oba';
