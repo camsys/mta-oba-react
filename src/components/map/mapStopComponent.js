bc4516866cb5d5c70074a3afcbe59e730e8a505f
caylasavitzky <caysavitzky@gmail.com>
2024-06-07 10:25:21 -0400

    adding base icon

--- src/components/map/mapStopComponent.js
+++ src/components/map/mapStopComponent.js
@@ -9,20 +9,28 @@ const COMPONENT_IDENTIFIER = "mapStopComponent"
 
 function mapStopComponent  (stopData) {
     OBA.Util.trace('generating mapStopComponent: ' + stopData.id)
+    // let stopImageUrl = "img/stop/stop-" + stopData?.directionKey==null? "unknown":stopData.directionKey + ".png"
+    let stopImageUrl = "img/stop/stop-" + "unknown" + ".png"
+    console.log(stopImageUrl)
+
     let icon = L.icon({
-        iconUrl: stopMapIcon,
+        iconUrl: stopImageUrl,
         className: "svg-icon",
-        iconSize: [24, 40],
-        iconAnchor: [12, 40]
+        iconSize: [21, 21],
+        iconAnchor: [0, 0]
     })
-    let out = (<Marker position={stopData.longLat} key={COMPONENT_IDENTIFIER+"_"+stopData.id} icon={icon} id={COMPONENT_IDENTIFIER+"_"+stopData.id}>
-        <Popup key={stopData.id} className="map-popup">
-            <img src={stopPopupIcon} alt="busstop icon" className="icon"/>
-            <div className="popup-info">
-                <span className="name">{stopData.name}</span>
-                <span className="stop-code">{"Stopcode "+stopData.id}</span>
-            </div>
-        </Popup>
+
+
+
+    let out = (
+        <Marker position={stopData.longLat} key={COMPONENT_IDENTIFIER+"_"+stopData.id} icon={icon} id={COMPONENT_IDENTIFIER+"_"+stopData.id}>
+            <Popup key={stopData.id} className="map-popup">
+                <img src={stopPopupIcon} alt="busstop icon" className="icon"/>
+                <div className="popup-info">
+                    <span className="name">{stopData.name}</span>
+                    <span className="stop-code">{"Stopcode "+stopData.id}</span>
+                </div>
+            </Popup>
     </Marker>);
 
     return out
