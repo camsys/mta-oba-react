479aae8b690f049afecbaf65217b401f9345cda3
caylasavitzky <caysavitzky@gmail.com>
2024-07-12 15:52:18 -0400

    map centers on location or stop search

--- src/components/map/MapComponent.js
+++ src/components/map/MapComponent.js
@@ -17,96 +17,6 @@ import siriEffect from "../../js/updateState/siriEffect";
 
 
 
-// export const MapComponent = () =>{
-//         updateVehicles()
-//         OBA.Util.log("generating map")
-//
-//         const { vehicleState} = useContext(VehicleStateContext);
-//         const { state} = useContext(CardStateContext);
-//         let mapRouteComponents = []
-//         let mapStopComponents = []
-//         let mapVehicleComponents = []
-//         const processRoute = (route)=>{
-//             console.log("processing route for map: ",route)
-//             console.log("vehicle state:", vehicleState)
-//             let routeId = route.routeId.split("_")[1]
-//             console.log("using abbreviated routeId ",routeId)
-//             let vehicleComponentsForRoute = vehicleState[routeId+"_mapVehicleComponents"]
-//             console.log("found these vehicle components", routeId,vehicleComponentsForRoute)
-//             if(vehicleComponentsForRoute!=null){
-//                 vehicleComponentsForRoute.forEach(x=>{mapVehicleComponents.push(x)});
-//             }
-//             route.directions.forEach(dir=>{
-//                 dir.mapRouteComponentData.forEach((datum)=>{
-//                     console.log("requesting new MapRouteComponent from: ",datum)
-//                     mapRouteComponents.push(new MapRouteComponent(datum))
-//                 })
-//                 dir.mapStopComponentData.forEach((datum)=>{
-//                     mapStopComponents.push(new MapStopComponent(datum))
-//                 })
-//             })
-//         }
-//         {state.currentCard.searchMatches.forEach(searchMatch=>{
-//             if(searchMatch.type=="routeMatch"){
-//                 let route = searchMatch
-//                 processRoute(route)
-//             }
-//             if(searchMatch.type=="geocodeMatch") {
-//                 searchMatch.nearbyRoutes.forEach(route => {processRoute(route)})
-//             }
-//             if(searchMatch.type=="stopMatch") {
-//                 searchMatch.routesAvailable.forEach(route => {processRoute(route)})
-//             }
-//         })}
-//
-//         console.log("map route components")
-//         console.log(mapRouteComponents)
-//         console.log("map vehicle components")
-//         console.log(mapVehicleComponents)
-//         console.log("map stop components")
-//         console.log(mapStopComponents)
-//
-//         const [Zoom, setZoom] = useState(11);
-//
-//
-//         const MapEvents = () => {
-//             let map = useMap()
-//             useMapEvents({
-//                 zoomend() { // zoom event (when zoom animation ended)
-//                     console.log("tryna use map")
-//                     console.log("map used")
-//                     const zoom = map.getZoom(); // get current Zoom of map
-//                     setZoom(zoom);
-//                 },
-//             });
-//             return false;
-//         };
-//
-//
-//         return (
-//                 <React.Fragment>
-//                     <MapContainer
-//                         style={{height: '100vh', width: '100wh'}}
-//                         center={OBA.Config.defaultMapCenter}
-//                         zoom={Zoom} scrollWheelZoom={true}
-//
-//                     >
-//                         <ReactLeafletGoogleLayer
-//                             apiKey='AIzaSyA-PBbsL_sXOTfo2KbkVx8XkEfcIe48xzw'
-//                             type={'roadmap'}
-//                             styles={OBA.Config.mutedTransitStylesArray}
-//                         />
-//                         <MapEvents />
-//
-//                         {mapRouteComponents}
-//                         {mapVehicleComponents}
-//                         { Zoom >= 15.1 ? mapStopComponents:null }
-//                     </MapContainer>
-//                 </React.Fragment>)
-//         }
-
-
-
 const MapVehicleElements = () =>{
     const { state} = useContext(CardStateContext);
     let routeIds = state.currentCard.routeIdList
@@ -136,8 +46,12 @@ const MapVehicleElements = () =>{
     )
 }
 
-const MapElements = React.memo(() => {
+export const MapComponent = () => {
+
     OBA.Util.log("generating map")
+    const [Zoom, setZoom] = useState(11);
+    let mapCenter = OBA.Config.defaultMapCenter;
+    // const [mapCenter, setMapCenter] = useState(OBA.Config.defaultMapCenter)
     const { state} = useContext(CardStateContext);
     let mapRouteComponents = []
     let mapStopComponents = []
@@ -160,17 +74,22 @@ const MapElements = React.memo(() => {
             processRoute(route)
         }
         if(searchMatch.type=="geocodeMatch") {
+            mapCenter = [searchMatch.latitude,searchMatch.longitude]
+            // setMapCenter([searchMatch.latitude,searchMatch.longitude])
             searchMatch.nearbyRoutes.forEach(route => {processRoute(route)})
         }
         if(searchMatch.type=="stopMatch") {
+            mapCenter = [searchMatch.latitude,searchMatch.longitude]
+            // setMapCenter([searchMatch.latitude,searchMatch.longitude])
             searchMatch.routesAvailable.forEach(route => {processRoute(route)})
         }
     })}
 
     console.log("map route components", mapRouteComponents)
     console.log("map stop components", mapStopComponents)
+    console.log("map center set to: ",mapCenter)
 
-    const [Zoom, setZoom] = useState(11);
+
 
 
     const MapEvents = () => {
@@ -187,20 +106,11 @@ const MapElements = React.memo(() => {
     };
     return (
         <React.Fragment>
-                <MapEvents />
-                {mapRouteComponents}
-                {Zoom >= 15.1 ? mapStopComponents : null}
-        </React.Fragment>
-    );
-});
-
-export const MapComponent = () => {
-    return (
-        <React.Fragment>
             <MapContainer
                 style={{ height: '100vh', width: '100wh' }}
-                center={OBA.Config.defaultMapCenter}
-                zoom={11}
+                // center={mapCenter}
+                center={mapCenter}
+                zoom={Zoom}
                 scrollWheelZoom={true}
             >
                 <ReactLeafletGoogleLayer
@@ -208,7 +118,9 @@ export const MapComponent = () => {
                     type={'roadmap'}
                     styles={OBA.Config.mutedTransitStylesArray}
                 />
-                <MapElements/>
+                <MapEvents />
+                {mapRouteComponents}
+                {Zoom >= 15.1 ? mapStopComponents : null}
                 <MapVehicleElements/>
             </MapContainer>
         </React.Fragment>
