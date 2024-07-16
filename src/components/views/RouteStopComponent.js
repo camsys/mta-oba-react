1c166f4869a7080ee4eb50eebdf9ac6c354415f2
Becky Soll <beckysoll@gmail.com>
2024-07-16 11:33:28 -0400

    the has-info class should only be added to an li if it has extra info about an approaching bus at that stop.

--- src/components/views/RouteStopComponent.js
+++ src/components/views/RouteStopComponent.js
@@ -4,7 +4,7 @@ import RouteVehicleComponent from "./routeVehicleComponent"
 function getRouteStopComponent  (stopData) {
     console.log("generating RouteStopComponent for ",stopData)
     return (
-        <li className="has-info" key={stopData.name + " " + stopData.id}>
+        <li key={stopData.name + " " + stopData.id}>
             <a href={stopData.longLat[0] + "," + stopData.longLat[1]} tabIndex={stopData.id}>{stopData.name}</a>
         </li>
     )
