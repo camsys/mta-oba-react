ca1841f60ca89a0c3aabb3f1905535bac4bde9f1
caylasavitzky <caysavitzky@gmail.com>
2024-07-16 21:13:20 -0400

    undoing my misguided apicall optimization  p2

--- src/js/updateState/siriEffect.js
+++ src/js/updateState/siriEffect.js
@@ -15,53 +15,60 @@ import {VehicleCardContentComponent} from "../../components/views/VehicleCard";
 
 
 const siriEffect = (routeId) => {
+    let operatorRef = routeId.split("_")[0].replace(" ","+");
+    const lineRef = routeId.split("_")[1];
 
 
-    function extractData (siri,dataSpecifiers){
-        // let listParser = siri?.Siri?.ServiceDelivery?.VehicleMonitoringDelivery[0]?.VehicleActivity
-        // let indivParser = objList[i].MonitoredVehicleJourney
+    function extractData (siri){
+        console.log("extractData")
+        dataSpecifiers.keyword = "serviceAlert & vehicle"
         let classList = dataSpecifiers.classList
         let vehicleActivity = siri?.Siri?.ServiceDelivery?.VehicleMonitoringDelivery[0]?.VehicleActivity
         OBA.Util.log(dataSpecifiers.keyword+" found:")
         OBA.Util.log(vehicleActivity)
+
+        let [vehicleDataList,serviceAlertDataList,serviceAlertComponents,mapVehicleComponents,routeVehicleComponents] = [[],[],[],[],[]]
+        console.log([vehicleDataList,serviceAlertDataList,serviceAlertComponents,mapVehicleComponents,routeVehicleComponents])
         if (vehicleActivity != null && vehicleActivity.length != 0) {
             update = true;
             for (let i = 0; i < vehicleActivity.length; i++) {
                 OBA.Util.trace("processing "+dataSpecifiers.keyword+"#" + i);
                 // let obj= new classList.dataClass(indivParser(jsonList,i))
-                let obj= new classList.dataClass(vehicleActivity[i].MonitoredVehicleJourney)
-                classList.dataContainer.push(obj)
-                classList.otherClasses.forEach((c)=>{
-                    c.container.push(new c.targetClass(obj, i))
-                })
+                let mvj = vehicleActivity[i].MonitoredVehicleJourney
+                vehicleDataList.push(new vehicleData(mvj))
+                serviceAlertDataList.push(new serviceAlertData(mvj))
+                serviceAlertComponents.push(new serviceAlertComponent(new serviceAlertData(mvj)))
+                mapVehicleComponents.push(new mapVehicleComponent(new vehicleData(mvj)))
+                routeVehicleComponents.push(new routeVehicleComponent(new vehicleData(mvj)))
             };
-
+            console.log([vehicleDataList,serviceAlertDataList,serviceAlertComponents,mapVehicleComponents,routeVehicleComponents])
             OBA.Util.log('processed '+dataSpecifiers.keyword)
         } else {
             OBA.Util.log('no '+dataSpecifiers.keyword+' recieved. not processing '+dataSpecifiers.keyword)
         }
         OBA.Util.log(dataSpecifiers.keyword+" post process")
-        return dataSpecifiers
+        return [vehicleDataList,serviceAlertDataList,serviceAlertComponents,mapVehicleComponents,routeVehicleComponents]
     }
 
-    function updateState(dataSpecifiers){
-        let classList = dataSpecifiers.classList
-        OBA.Util.log("should update "+dataSpecifiers.keyword+" state?")
-        OBA.Util.log(update)
+
+    function updateState([vehicleDataList,serviceAlertDataList,serviceAlertComponents,mapVehicleComponents,routeVehicleComponents]){
+        // let classList = dataSpecifiers.classList
+        console.log("should update serviceAlert & vehicle state?",update)
         if(update) {
-            OBA.Util.log("adding to "+dataSpecifiers.keyword+" state:")
-            let capitalkeyword = dataSpecifiers.keyword.substring(0, 1).toUpperCase() + dataSpecifiers.keyword.substring(1);
+            console.log("adding to serviceAlert & vehicle state:",[vehicleDataList,serviceAlertDataList,serviceAlertComponents,mapVehicleComponents,routeVehicleComponents])
+            // let capitalkeyword = dataSpecifiers.keyword.substring(0, 1).toUpperCase() + dataSpecifiers.keyword.substring(1);
             let stateFunc = (prevState) => {
                 let newState = {...prevState}
-                newState[dataSpecifiers.keyword+"Data"] = classList.dataContainer
-                classList.otherClasses.forEach((c)=>{
-                    newState[c.identifier + capitalkeyword + "Components"] = c.container
-                })
+                newState[lineRef+"_serviceAlertData"] = vehicleData
+                newState[lineRef+"_vehicleAlertData"] = serviceAlertData
+                newState[lineRef+"_serviceAlertComponents"]=serviceAlertComponents
+                newState[lineRef+"_mapVehicleComponents"]=mapVehicleComponents
+                newState[lineRef+"_routeVehicleComponents"]=routeVehicleComponents
                 return newState
             }
             setState(stateFunc);
         }
-        OBA.Util.log("new "+dataSpecifiers.keyword+" state")
+        console.log("new serviceAlert & vehicle state",state)
 
     }
 
@@ -70,8 +77,7 @@ const siriEffect = (routeId) => {
 
     var keyword = "vehicle"
 
-    let operatorRef = routeId.split("_")[0].replace(" ","+");
-    const lineRef = routeId.split("_")[1];
+
     let search = "&OperatorRef=" +operatorRef + "&LineRef"+"=" + lineRef.replace("+","%2B");
     let targetAddress = "https://" + process.env.ENV_ADDRESS + "/" + process.env.VEHICLE_MONITORING_ENDPOINT + search
     console.log("searching for siri at: ",targetAddress)
@@ -91,9 +97,6 @@ const siriEffect = (routeId) => {
         new pathRouting((siri)=>{return siri?.Siri?.ServiceDelivery?.VehicleMonitoringDelivery[0]?.VehicleActivity},
             (objList,i)=>{return objList[i].MonitoredVehicleJourney}))
 
-
-
-
     const dataSpecifiersList = [vehicleSpecifiers,serviceAlertSpecifiers]
     const { setState } = useContext(VehicleStateContext);
     var update = false
@@ -107,13 +110,10 @@ const siriEffect = (routeId) => {
         fetch(targetAddress)
             .then((response) => response.json())
             .then((siri) => {
-                console.log(dataSpecifiersList)
-                dataSpecifiersList.forEach((dataSpecifiers)=>{
-                    console.log(dataSpecifiers)
-                    console.log(dataSpecifiers.keyword)
-                    OBA.Util.log("reading "+dataSpecifiers.keyword+" from " + targetAddress)
-                    updateState(extractData(siri,dataSpecifiers))
-                })
+                OBA.Util.log("reading serviceAlert & vehicle from " + targetAddress)
+                updateState(extractData(siri))
+                // extractData(siri)
+
             })
             .catch((error) => {
                 console.error(error);
