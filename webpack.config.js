c147060ffb464f0d7cf66c5be4da39d4839879b0
caylasavitzky <caysavitzky@gmail.com>
2024-07-29 21:06:11 -0400

    moving to dev

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const htmlPlugin = new HtmlWebPackPlugin({
 });
 const envPlugin = new webpack.DefinePlugin({
   'process.env.ALLOWED_HOST_ADDRESS': JSON.stringify(process.env.ALLOWED_HOST_ADDRESS || 'localhost'),
-  'process.env.ENV_ADDRESS': JSON.stringify(cleanUpHostAddress(process.env.ENV_ADDRESS || 'app.qa.obanyc.com')),
+  'process.env.ENV_ADDRESS': JSON.stringify(cleanUpHostAddress(process.env.ENV_ADDRESS || 'app.dev.obanyc.com')),
   'process.env.VEHICLE_MONITORING_ENDPOINT': JSON.stringify(process.env.VEHICLE_MONITORING_ENDPOINT || 'api/siri/vehicle-monitoring.json?key=OBANYC'),
   'process.env.STOPS_ON_ROUTE_ENDPOINT': JSON.stringify(process.env.STOPS_ON_ROUTE_ENDPOINT || 'api/stops-on-route-for-direction?')
 });
