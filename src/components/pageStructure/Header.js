f2888f1e41e06821d54415d6a8df4e050e0f0704
Becky Soll <beckysoll@gmail.com>
2024-08-16 16:17:14 -0400

    text it page, breadcrumb

--- src/components/pageStructure/Header.js
+++ src/components/pageStructure/Header.js
@@ -19,7 +19,7 @@ function Header  () {
                 <nav>
                     <ul className="menu">
                         <li><a href="/" onClick={() => search("")}>Home</a></li>
-                        <li><a href="/text">Text</a></li>
+                        <li><a href="/help/text">Text</a></li>
                         <li><a href="/about">About</a></li>
                         <li><a href="/contact">Contact</a></li>
                         <li><a href="/wiki">Developers</a></li>
