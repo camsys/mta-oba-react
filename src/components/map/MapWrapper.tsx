edaedd1b674a51ea8e71b2512d1c32be45cd6c4b
Becky Soll <beckysoll@gmail.com>
2024-08-30 14:47:43 -0400

    mobile refresh button shows up where it needs to but needs .button class in that context only

--- src/components/map/MapWrapper.tsx
+++ src/components/map/MapWrapper.tsx
@@ -4,7 +4,9 @@ import {MapComponent} from './MapComponent';
 import ErrorBoundary from "../util/errorBoundary";
 import ReactLeafletGoogleLayer from "react-leaflet-google-layer";
 import RefreshComponent from "../views/RefreshComponent";
-
+import {useContext} from "react";
+import {CardStateContext} from "../util/CardStateComponent.tsx";
+import {Card, CardType} from "../../js/updateState/DataModels";
 
 
 
@@ -23,13 +25,15 @@ function getMapWrap  () : JSX.Element {
             return mapVisible ? 'Toggle Map Visibility (currently visible)' : 'Toggle Map Visibility (currently hidden)'
     }
 
+    const { state } = useContext(CardStateContext);
+
 
     OBA.Util.log("adding map-wrapper")
     return(
         <ErrorBoundary>
             <div id="map-wrap">
                 <div className="bottom-buttons" id="map-trigger-wrao">
-                    <RefreshComponent/>
+                    {state.currentCard.type === CardType.HomeCard ? null : <RefreshComponent/>}
                     <button id="map-toggle" className="button" aria-controls="map" aria-expanded={mapVisible} aria-pressed={mapVisible}
                             aria-label={ariaLabel} onClick={mapToggle}><span className="label"><span className="hide-label">Hide </span>Map</span>
                     </button>
