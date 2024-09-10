3f14aec1399e7898c3fe27ea08768bf4d78d53d0
Becky Soll <beckysoll@gmail.com>
2024-09-10 13:25:29 -0400

    no favorites language better matches what is on mta.info and looks good

--- src/components/pageStructure/FavoritesMenu.tsx
+++ src/components/pageStructure/FavoritesMenu.tsx
@@ -83,7 +83,7 @@ export const FavoritesMenu = () => {
         </button>
         <ul className="sub-menu menu collapse-content" id="favorites-menu" style={{ maxHeight: '0px' }} role="menu">
             {favoritesState.favorites.length == 0
-                ?<p>no favorites found</p>
+                ?<li class="no-favorites"><em>No favorites found.</em> <br></br>Your favorite bus stops and bus routes will be listed&nbsp;here.</li>
                 :favoritesState.favorites.map((datum,index)=>{return <FavoriteItem datum={datum} key = {index}/>})}
         </ul>
     </li>)
