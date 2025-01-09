da92a26e0f03ebcb39648db15027aa9f7abd0d33
Becky Soll <beckysoll@gmail.com>
2025-01-09 14:13:16 -0500

    rearranging bus list item, 10 meeples become 4, bus num moves to right side

--- src/components/views/VehicleComponent.tsx
+++ src/components/views/VehicleComponent.tsx
@@ -13,9 +13,6 @@ function VehicleComponent({vehicleDatum,tabbable}:
     let hasArrivalData = typeof vehicleDatum?.vehicleArrivalData!=='undefined'
     return(
         <li key={vehicleDatum.vehicleId}>
-            <a href="#" tabIndex={tabbable?0:-1}
-               onClick={()=>{vehicleSearch(vehicleDatum)}}
-               className={vehicleDatum?.strollerVehicle?"bus stroller-friendly":"bus"}>{vehicleDatum.vehicleId.split("_")[1]}</a>
             <span className="bus-info">
                 <span className="approaching">
                     {OBA.Util.getArrivalEstimateForISOString(vehicleDatum?.vehicleArrivalData?.[0].ISOTime,vehicleDatum.lastUpdate)}
@@ -26,15 +23,18 @@ function VehicleComponent({vehicleDatum,tabbable}:
                 {vehicleDatum.passengerCount != null && (
                     vehicleDatum.passengerCapacity != null ?
                         <span className="passengers">
-                            <span className={'meeples meeples-'+`${Math.ceil((vehicleDatum.passengerCount / vehicleDatum.passengerCapacity) * 10)}`}>
+                            <span className={'meeples meeples-'+`${Math.ceil((vehicleDatum.passengerCount / vehicleDatum.passengerCapacity) * 4)}`}>
                                 <img src={meeples} alt={`vehicle is ~${Math.ceil((vehicleDatum.passengerCount / vehicleDatum.passengerCapacity) * 100)}% full`} title={`vehicle is ~${Math.ceil((vehicleDatum.passengerCount / vehicleDatum.passengerCapacity) * 100)}% full`} className="meeples-blank" />
                             </span>
+                            {vehicleDatum.passengerCount != null
+                                ?(<span class="passenger-count">{`~${vehicleDatum.passengerCount} passengers`}</span>)
+                                : null}
                         </span>
                         : null)}
-                {vehicleDatum.passengerCount != null
-                    ?(<span>{`~${vehicleDatum.passengerCount} passengers`}</span>)
-                    : null}
             </span>
+            <a href="#" tabIndex={tabbable?0:-1}
+               onClick={()=>{vehicleSearch(vehicleDatum)}}
+               className={vehicleDatum?.strollerVehicle?"bus stroller-friendly":"bus"}>{vehicleDatum.vehicleId.split("_")[1]}</a>
         </li>
     )
 
