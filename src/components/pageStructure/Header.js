3420bf337b9feb3abf45484879b894fcb149771e
Becky Soll <beckysoll@gmail.com>
2025-04-28 16:48:54 -0400

    mta logo looking good

--- src/components/pageStructure/Header.js
+++ src/components/pageStructure/Header.js
@@ -18,22 +18,24 @@ function Header  () {
                 <a href="/about-beta">Learn More and Provide Feedback</a>
             </div>
             <header className="header" id="header">
-                <a href="#" onClick={() => search("")} aria-label="MTA Bus Time Home" className="logo-link">
-                    <img src={bustimeLogo} alt="MTA Bus Time" className="logo" />
-                </a>
+                <div className="header-main">
+                    <a href="#" onClick={() => search("")} aria-label="MTA Bus Time Home" className="logo-link">
+                        <img src={bustimeLogo} alt="MTA Bus Time" className="logo" />
+                    </a>
+                    <nav>
+                        <ul className="menu">
+                            <li><a href="#" onClick={() => search("")}>Home</a></li>
+                            <li><a href="/help/text">Text</a></li>
+                            <li><a href="/about">About</a></li>
+                            <li><a href="/contact">Contact</a></li>
+                            <li><a href="/developers">Developers</a></li>
+                            <li><a href="/help">Help</a></li>
+                        </ul>
+                    </nav>
+                </div>
                 <a href="https://www.mta.info/" target="_blank" aria-label="MTA Home" className="mta-logo-link">
                     <img src={mtaLogo} alt="MTA" className="mta-logo" />
                 </a>
-                <nav>
-                    <ul className="menu">
-                        <li><a href="#" onClick={() => search("")}>Home</a></li>
-                        <li><a href="/help/text">Text</a></li>
-                        <li><a href="/about">About</a></li>
-                        <li><a href="/contact">Contact</a></li>
-                        <li><a href="/developers">Developers</a></li>
-                        <li><a href="/help">Help</a></li>
-                    </ul>
-                </nav>
             </header>
         </ErrorBoundary>
     )
