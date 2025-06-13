2077d179126cf124288a7cba0e78830bd086e689
Becky Soll <beckysoll@gmail.com>
2025-06-13 13:20:46 -0400

    error message styling
    sass tweak due to deprecation

--- src/components/views/ErrorCard.tsx
+++ src/components/views/ErrorCard.tsx
@@ -1,13 +1,11 @@
 import React, {useContext} from 'react';
 export function ErrorCard({errorMessage}: { errorMessage: string }): JSX.Element {
     return (<React.Fragment>
-                <h2 className={`cards-header`}>Search could not be completed</h2>
-                <div className="card error-card">
-                    <p>
-                        Please confirm that there were no typos in your search request.
-                        Using the autosuggest feature can help avoid this.
-                    </p>
-                </div>
+                <h2 className={`error-header`}>Search could not be completed</h2>
+                <p>
+                    Please confirm that there were no typos in your search request.
+                    Try the autosuggest feature in the search bar, or <a href="/?search=allRoutes">view all available routes here</a>.
+                </p>
             </React.Fragment>
     );
 }
