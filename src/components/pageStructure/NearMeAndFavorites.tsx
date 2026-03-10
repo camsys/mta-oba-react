6edeb41305a44ae8bc168e607429eb09e1c35aa1
CaylaSavitzky <CaySavitzky@gmail.com>
2026-03-10 12:11:34 -0400

    format and center favorites+nearme navigation

--- src/components/pageStructure/NearMeAndFavorites.tsx
+++ src/components/pageStructure/NearMeAndFavorites.tsx
@@ -5,17 +5,18 @@ import { FavoritesButton,NearMeButton } from '../shared/buttons';
 
 function NearMeAndFavoritesComponent  () : JSX.Element {
 
-    const nearMeText = (
-        <>
-            <span className='hidden min-[420px]:inline min-[600px]:hidden min-[900px]:inline'>Routes & Stops </span>
-            <span className=''>Near Me</span>
-        </>
-    )
+    // TODO: get confirmation i can remove this
+    // const nearMeText = (
+    //     <>
+    //         <span className='hidden min-[420px]:inline min-[600px]:hidden min-[900px]:inline'>Routes & Stops </span>
+    //         <span className=''>Near Me</span>
+    //     </>
+    // )
 
     log.info("adding NearMeAndFavorites")
-    return (<div className="py-3 px-2 flex justify-center gap-4">
-        <FavoritesButton className='py-[.66rem] basis-[0] ' iconClassName='w-5 h-5' textClassName='text-base truncate'/>
-        <NearMeButton className='py-[.66rem] basis-[0] ' iconClassName='w-5 h-5' textClassName='text-base truncate' text="Nearby Buses"/>
+    return (<div className="max-[360px]:gap-2 items-stretch py-3 px-2 flex justify-center gap-4">
+        <FavoritesButton className='flex max-w-[200px] justify-center py-[.66rem] basis-[0] flex-1' iconClassName='mb-1 w-5 h-5' textClassName='text-base truncate'/>
+        <NearMeButton className='flex max-w-[200px] justify-center py-[.66rem] basis-[0] flex-1' iconClassName='mb-1 w-5 h-5' textClassName='text-base truncate' text="Nearby Buses"/>
         </div>);
 }
 
