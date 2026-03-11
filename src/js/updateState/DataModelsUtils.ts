42429aa97f208dad25f5de22be6f39e870b937c4
CaylaSavitzky <CaySavitzky@gmail.com>
2026-03-11 10:51:48 -0400

    minor update to interface support tool

--- src/js/updateState/DataModelsUtils.ts
+++ src/js/updateState/DataModelsUtils.ts
@@ -9,7 +9,9 @@ export const isStopInterface = (obj: any): obj is StopInterface =>
     typeof obj.longLat?.[0] === "number" &&
     typeof obj.longLat?.[1] === "number" &&
     typeof obj?.id === "string" &&
-    typeof obj?.stopDirection === "string";
+    typeof obj?.stopDirection === "string" &&
+    typeof obj?.datumId === "string" &&
+    typeof obj?.datumName === "string";
 
 export const isRouteInterface = (obj: any): obj is RouteInterface =>
     typeof obj === "object" &&
@@ -17,12 +19,15 @@ export const isRouteInterface = (obj: any): obj is RouteInterface =>
     typeof obj?.color === "string" &&
     typeof obj?.routeId === "string" &&
     typeof obj?.routeTitle === "string" &&
-    (typeof obj?.description === "string" || obj?.description === undefined || obj?.description === null);
+    (typeof obj?.description === "string" || obj?.description === undefined || obj?.description === null) &&
+    typeof obj?.datumId === "string" &&
+    typeof obj?.datumName === "string";
+
 
 export const extractRouteInterface = (obj: any): RouteInterface | null => {
     if (isRouteInterface(obj)) {
-        const { color, routeId, routeTitle, description } = obj;
-        return { color, routeId, routeTitle, description };
+        const { color, routeId, routeTitle, description, datumId, datumName } = obj;
+        return { color, routeId, routeTitle, description, datumId, datumName };
     }
     return null;
 };
@@ -31,8 +36,8 @@ export const extractRouteInterface = (obj: any): RouteInterface | null => {
 
 export const extractStopInterface = (obj: any): StopInterface | null => {
     if (isStopInterface(obj)) {
-        const { name, longLat, id, stopDirection } = obj;
-        return { name, longLat, id, stopDirection };
+        const { name, longLat, id, stopDirection, datumId, datumName } = obj;
+        return { name, longLat, id, stopDirection, datumId, datumName };
     }
     return null;
 };
