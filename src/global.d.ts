b37e8cf61149325db3c6d7a72953532a88350d79
CaylaSavitzky <CaySavitzky@gmail.com>
2026-04-24 03:05:47 -0400

    ts alignment changes

---
+++ src/global.d.ts
@@ -0,0 +1,15 @@
+declare module "*.svg" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.gif";
+declare module "*/oba";
+declare module "*/util/errorBoundary.js";
+declare module "*/views/RefreshComponent.js";
+declare module "*/js/updateState/NavigationEffect.js";
+declare module "*/js/updateState/DataModels.js"
+declare module "*/pageStructure/SideBar"
