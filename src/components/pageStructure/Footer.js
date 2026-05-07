66c5a892a77c350d12ec1ba2f18fe73e6011bafb
Terry Guan <terryguan97@gmail.com>
2026-05-07 11:57:46 -0400

    NYCUI-558 align new ui footer and links with MTA request 2026-05-07

--- src/components/pageStructure/Footer.js
+++ src/components/pageStructure/Footer.js
@@ -17,10 +17,9 @@ function Footer  () {
         <ErrorBoundary>
             <GoogleTranslateButton/>
             <div className="footer text-white bg-mta-black-2" id="footer">
-                <h2 className='text-[1.35em] mt-4 mb-2 pt-[3px] font-bold'>MTA Bus Time</h2>
-                <section className="my-2">
+                <section className="my-4">
                     <div className="relative inline-block py-1 ">
-                        <div className="absolute top-0 left-1/8 w-3/4 border-t border-white/40 text-center"></div>
+                        <div className="absolute top-0 left-1/8 w-3/4 border-white/40 text-center"></div>
                         <UnderlineOnFocusElement href={classicSiteAddress} className="text-4 font-[575] my-1">
                             Bus Time Classic
                         </UnderlineOnFocusElement>
@@ -29,13 +28,13 @@ function Footer  () {
                 </section>
                 <nav>
                     <ul className="menu my-1 text-4 font-[575]">
-                        <li><UnderlineOnFocusElement href="//mta.info">MTA Homepage</UnderlineOnFocusElement></li>
+                        <li><UnderlineOnFocusElement href="//mta.info">MTA Website</UnderlineOnFocusElement></li>
                         <li><UnderlineOnFocusElement href="/help/text">Mobile Text Messages</UnderlineOnFocusElement></li>
+                        <li><UnderlineOnFocusElement href="//cloud.info.mta.org/create-mta-service-alerts?utm_source=MTA_Bus_Time&utm_medium=referral&utm_campaign=MTA_Service_Alerts">Sign Up For Service Alerts</UnderlineOnFocusElement></li>
                         <li><UnderlineOnFocusElement href="/about">About Bus Time</UnderlineOnFocusElement></li>
                         <li><UnderlineOnFocusElement href="/contact">Contact Us</UnderlineOnFocusElement></li>
-                        <li><UnderlineOnFocusElement href="/developers">Developers</UnderlineOnFocusElement></li>
-                        <li><UnderlineOnFocusElement href="/help">Help</UnderlineOnFocusElement></li>
                         <li><UnderlineOnFocusElement href="//www.mta.info/privacy-policy">Privacy Policy</UnderlineOnFocusElement></li>
+                        <li><UnderlineOnFocusElement href="//www.mta.info/terms-and-conditions">Terms & Conditions</UnderlineOnFocusElement></li>
                     </ul>
                 </nav>
             </div>
